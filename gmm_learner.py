bea2d9a96dff56b722948f6bf6e7e1da8acf5d2e
ColeConte <colefconte@gmail.com>
2020-07-03 21:38:25 -0700

    training one univariate normal per digit

--- gmm_learner.py
+++ gmm_learner.py
@@ -1,17 +1,45 @@
 '''
 Cole Conte CSE 512 Hw 3
+https://colab.research.google.com/drive/1Eb-G95_dd3XJ-0hm2qDqdtqMugLkSYE8#scrollTo=erwQND925xoe
+https://towardsdatascience.com/how-to-code-gaussian-mixture-models-from-scratch-in-python-9e7975df5252
 '''
 
+import argparse
+import pandas as pd
+import numpy as np
+import math
+
 def gmmLearner(train,test,components):
-	pass
+	for digit in range(10):
+		trainDigit = train[train.iloc[:,-1]==digit]
+		#Does standard normal work for starting params?
+		#Scale equally divided among components to start?
+		mu = 0.0
+		sigmaSq = 1.0
+		scale = 1.0
+		for row in range(len(trainDigit)):
+			#Implement MVN, starting with regular old Normal using col 1
+			#for k in range(len(components)):
+			x = trainDigit.iloc[row,1]
+			likelihood = math.exp(-((x-mu)**2)/(2*sigmaSq))/math.sqrt(2*math.pi*sigmaSq)
+			b = likelihood #this will change when we add multiple components
+			muold = mu
+			mu = ((mu * (row+1)) + x)/(row+2) #THIS ISNT WHAT THE SITE SAYS
+			sigmaSq = ((sigmaSq * (row+1)) + ((x-muold)**2))/(row+2) #THIS ISNT WHAT THE SITE SAYS
+			#update scale
+		print("For digit " + str(digit) + " mu="+str(mu) +" and sigmaSq=" + str(sigmaSq))
+	
+
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--components")
 parser.add_argument("--train")
 parser.add_argument("--test")
+args = parser.parse_args()
 
-train = pd.read_csv(args.train)
-test = pd.read_csv(args.test)
-gmmLearner(train,test,int(components))
+train = pd.read_csv(args.train,header=None)
+test = pd.read_csv(args.test,header=None)
+gmmLearner(train,test,int(args.components))
 
 
---
+++ readme.txt
@@ -0,0 +1,2 @@
+cd Documents/GitHub/gaussian-mixture-model
+python gmm_learner.py --components 1 --train optdigits.train --test optdigits.test
