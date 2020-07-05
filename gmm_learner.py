bb2e2b24dab6fd96a2b8132466139b458b991987
ColeConte <colefconte@gmail.com>
2020-07-04 22:46:02 -0700

    Update gmm_learner.py

--- gmm_learner.py
+++ gmm_learner.py
@@ -10,25 +10,30 @@ import numpy as np
 import math
 
 def gmmLearner(train,test,components):
+	#Train
 	for digit in range(10):
 		trainDigit = train[train.iloc[:,-1]==digit]
 		#Does standard normal work for starting params?
 		#Scale equally divided among components to start?
-		mu = 0.0
-		sigmaSq = 1.0
-		scale = 1.0
+		mu = [0.0]*(len(train.columns)-1) 
+		cov = np.zeros((len(train.columns)-1,len(train.columns)-1),int)
+		np.fill_diagonal(cov,1) #init as indpt
+		scale = [1.0]*(len(train.columns)-1) 
 		for row in range(len(trainDigit)):
 			#Implement MVN, starting with regular old Normal using col 1
 			#for k in range(len(components)):
-			x = trainDigit.iloc[row,1]
-			likelihood = math.exp(-((x-mu)**2)/(2*sigmaSq))/math.sqrt(2*math.pi*sigmaSq)
-			b = likelihood #this will change when we add multiple components
-			muold = mu
-			mu = ((mu * (row+1)) + x)/(row+2) #THIS ISNT WHAT THE SITE SAYS
-			sigmaSq = ((sigmaSq * (row+1)) + ((x-muold)**2))/(row+2) #THIS ISNT WHAT THE SITE SAYS
-			#update scale
+			x = trainDigit.iloc[row,:-1]
+			for col in range(len(train.columns)-1):
+				likelihood = math.exp(-((x[col]-mu[col])**2)/(2*sigmaSq[col]))/math.sqrt(2*math.pi*sigmaSq[col])
+				b = likelihood #this will change when we add multiple components
+				muold = mu[col]
+				mu[col] = ((mu[col] * (row+1)) + x[col])/(row+2) #THIS ISNT WHAT THE SITE SAYS
+				sigmaSq[col] = ((sigmaSq[col] * (row+1)) + ((x[col]-muold)**2))/(row+2) #THIS ISNT WHAT THE SITE SAYS
+				#update scale
 		print("For digit " + str(digit) + " mu="+str(mu) +" and sigmaSq=" + str(sigmaSq))
 	
+	#Test
+
 
 
 
